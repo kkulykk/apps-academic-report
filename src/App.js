6effece93c0785bf7d34bc774e0c4015c662c10f
Roman Kulyk <roman.kulyk@romankulyk.local>
2023-01-25 21:46:33 +0200

    Add images to alumni page and remove unnecessary media

--- src/App.js
+++ src/App.js
@@ -9,12 +9,8 @@ import StrategyPage from './pages/StrategyPage/StrategyPage';
 import ScrollUpBtn from './components/ScrollUpBtn/ScrollUpBtn';
 import AdmissionCampaignResPage from './pages/IntroCampaignResPage/IntroCampaignResPage';
 import StructureChangesPage from './pages/StructureChangesPage/StructureChangesPage';
-import StudentsWinsPage from './pages/StudentWinsPage/StudentsWinsPage';
-import ProfessorsWinsPage from './pages/ProfessorsWinsPage/ProfessorsWinsPage';
 import SocialProjectsPage from './pages/SocialProjectsPage';
-import CertificateProgramsPage from './pages/CertificateProgramsPage/CertificateProgramsPage';
 import SciencePage from './pages/SciencePage/SciencePage';
-import RatingsPage from './pages/RatingsPage/RatingsPage';
 import GraduatesPage from './pages/GraduatesPage';
 import WhoWeArePage from './pages/WhoWeArePage/WhoWeArePage';
 
@@ -29,12 +25,8 @@ const App = () => {
           <Route path="/strategy-goals" element={<StrategyPage />} />
           <Route path="/admission-campaign-results" element={<AdmissionCampaignResPage />} />
           <Route path="/new-members" element={<StructureChangesPage />} />
-          <Route path="/students-wins" element={<StudentsWinsPage />} />
-          <Route path="/professors-wins" element={<ProfessorsWinsPage />} />
           <Route path="/social-projects" element={<SocialProjectsPage />} />
-          <Route path="/certificate-programs" element={<CertificateProgramsPage />} />
           <Route path="/science" element={<SciencePage />} />
-          <Route path="/ratings" element={<RatingsPage />} />
           <Route path="/alumni" element={<GraduatesPage />} />
           <Route path="/who-we-are" element={<WhoWeArePage />} />
         </Routes>
--- src/pages/GraduatesPage/GraduatesPage.jsx
+++ src/pages/GraduatesPage/GraduatesPage.jsx
@@ -2,12 +2,17 @@ import { Trans, useTranslation } from 'react-i18next';
 
 import AppBar from '../../components/AppBar/AppBar';
 import Container from '../../components/Container';
+import { getLanguageImage } from '../../utils/services';
 
 import styles from './GraduatesPage.module.css';
 
 const images = {
   alumni:
-    'https://firebasestorage.googleapis.com/v0/b/academic-report.appspot.com/o/images%2Falumni%2Falumni.jpg?alt=media&token=31ae868e-fa32-4c38-bce6-e27e9f6650bc'
+    'https://firebasestorage.googleapis.com/v0/b/academic-report.appspot.com/o/images%2Falumni%2Falumni.jpg?alt=media&token=31ae868e-fa32-4c38-bce6-e27e9f6650bc',
+  furtherStudies: {
+    en: 'https://firebasestorage.googleapis.com/v0/b/academic-report.appspot.com/o/images%2Falumni%2FfurtherStudiesEN.png?alt=media&token=3a3785a8-59e4-4d43-b8e8-4592e618c356',
+    uk: 'https://firebasestorage.googleapis.com/v0/b/academic-report.appspot.com/o/images%2Falumni%2FfurtherStudiesUK.png?alt=media&token=7ca39d4c-b294-4eea-845d-832bb3c0a313'
+  }
 };
 
 const GraduatesPage = () => {
@@ -23,15 +28,17 @@ const GraduatesPage = () => {
               <img src={images.alumni} alt="happy students" />
             </picture>
           </div>
-
           <p className={styles.description}>
             <Trans i18nKey={'pages.graduates.text'} components={[<br />]} />
           </p>
           <div className={styles.imgWrapper}>
-            {/*<picture>*/}
-            {/*  <source srcSet={grad1ImgBig} media="(min-width:768px)" />*/}
-            {/*  <img src={grad1ImgMedium} className={styles.img} alt="APPS UCU ALUMNI" />*/}
-            {/*</picture>*/}
+            <picture>
+              <img
+                src={getLanguageImage(images.furtherStudies)}
+                className={styles.img}
+                alt="Where alumni continue studying"
+              />
+            </picture>
           </div>
         </section>
       </Container>
