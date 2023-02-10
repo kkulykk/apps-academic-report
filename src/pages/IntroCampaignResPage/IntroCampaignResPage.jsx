6a09363889733733bf90a8af836ed09e5d158141
Bohdan Mykhayliv <bohdan.mykhayliv@bohdanmykhayliv.local>
2023-02-10 15:25:05 +0200

    Add Masters admission results

--- src/pages/IntroCampaignResPage/IntroCampaignResPage.jsx
+++ src/pages/IntroCampaignResPage/IntroCampaignResPage.jsx
@@ -5,9 +5,6 @@ import AppBar from '../../components/AppBar/AppBar';
 import Container from '../../components/Container';
 import { getLanguageImage } from '../../utils/services';
 
-import mobImg1 from '../../imgs/mobile/ukraine_mob.jpg';
-import deskImg1 from '../../imgs/desktop/ukraine.jpg';
-
 import styles from './IntroCampaignResPage.module.css';
 import 'react-medium-image-zoom/dist/styles.css';
 
@@ -17,8 +14,8 @@ const images = {
     en: 'https://firebasestorage.googleapis.com/v0/b/academic-report.appspot.com/o/images%2Fadmission-campaign%2FadmissionCampaignBachelorsEN.svg?alt=media&token=53bba618-0fb3-40ef-b376-a0fdafa1ad87'
   },
   masters: {
-    uk: '',
-    en: ''
+    uk: 'https://firebasestorage.googleapis.com/v0/b/academic-report.appspot.com/o/images%2Fadmission-campaign%2FadmissionCampaignMastersUK.svg?alt=media&token=a4f0487e-b5b9-4e06-85b1-4dbb3fb2f8a0',
+    en: 'https://firebasestorage.googleapis.com/v0/b/academic-report.appspot.com/o/images%2Fadmission-campaign%2FadmissionCampaignMastersEN.svg?alt=media&token=94a5975d-ad1f-4c51-94ac-9e6bd5423406'
   }
 };
 
@@ -34,7 +31,7 @@ const AdmissionCampaignResPage = () => {
             <Zoom>
               <picture>
                 <source srcSet={getLanguageImage(images.bachelors)} media="(min-width:768px)" />
-                <img src={getLanguageImage(images.bachelors)} className={styles.img} alt="Ukraine map" />
+                <img src={getLanguageImage(images.bachelors)} className={styles.img} alt="Bachelors admission results" />
               </picture>
             </Zoom>
           </div>
@@ -42,8 +39,8 @@ const AdmissionCampaignResPage = () => {
           <div className={styles.imgWrapper}>
             <Zoom>
               <picture>
-                <source srcSet={deskImg1} media="(min-width:768px)" />
-                <img src={mobImg1} className={styles.img} alt="Ukraine map" />
+              <source srcSet={getLanguageImage(images.masters)} media="(min-width:768px)" />
+                <img src={getLanguageImage(images.masters)} className={styles.img} alt="Masters admission results" />
               </picture>
             </Zoom>
           </div>
